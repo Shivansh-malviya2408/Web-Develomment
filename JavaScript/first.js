8648b6bd7b75af313c79212d2765b9a7186e2aa8
Shivansh Malviya <148011317+Shivansh-malviya2408@users.noreply.github.com>
2024-07-11 02:22:02 +0530

    JavaScript

---
+++ JavaScript/first.js
@@ -0,0 +1,35 @@
+
+// Used to Print in JavaScript
+console.log('Hello Kabir');
+
+// Creating of variable and defining data types
+
+// By var Keywords
+
+var a=5
+// console.log(a);
+
+var a=10
+console.log(a);
+
+// By let Keyword
+
+let b='JavaScript'
+// console.log(b);
+b='KabirJavaScript'
+console.log(b);
+
+
+// By const Keyword
+
+const c=false
+console.log(c);
+
+// Back-Tick
+
+let firstName='Your'
+let lastName='Kabir'
+let msg='hehehehehe'
+
+console.log(` my First name is ${firstName} \n & \n Second name is  ${lastName} \n message is ${msg}`);
+
